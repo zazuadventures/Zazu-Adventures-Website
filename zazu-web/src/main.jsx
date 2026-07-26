884dceafda19c518ae8e5effdfe327f300e9f05b
Davies Gotosa <codewithdaviess@gmail.com>
2026-07-26 11:18:09 +0200

    feat: add Vercel Analytics for improved tracking and insights

--- zazu-web/src/main.jsx
+++ zazu-web/src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
@@ -8,6 +9,7 @@ createRoot(document.getElementById("root")).render(
   <HelmetProvider>
     <StrictMode>
       <App />
+      <Analytics />
     </StrictMode>
   </HelmetProvider>,
 );
