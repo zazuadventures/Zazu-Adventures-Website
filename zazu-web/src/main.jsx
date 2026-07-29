949556005510cbf4002ff229f8706c70598e8600
Davies Gotosa <codewithdaviess@gmail.com>
2026-07-29 22:43:41 +0200

    fix: correct import path for SpeedInsights component from @vercel/speed-insights

--- zazu-web/src/main.jsx
+++ zazu-web/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
